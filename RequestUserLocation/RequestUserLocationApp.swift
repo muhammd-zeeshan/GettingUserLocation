6832b182226ea38926b70e66675e91d7c1a2eec3
muhmmad-zeeshan <rafaq657@gmail.com>
2024-09-14 22:02:12 +0500

    Initial Commit

---
+++ RequestUserLocation/RequestUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestUserLocationApp.swift
+//  RequestUserLocation
+//
+//  Created by Muhammad Zeeshan on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct RequestUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
